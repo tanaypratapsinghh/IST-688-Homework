c2f3e6578d1e3994950c0a1b87a24285186afab3
tanaypratapsinghh <tsingh13@syr.edu>
2026-09-06 23:47:53 +0000

    up 6

--- HW/HW2.py
+++ HW/HW2.py
@@ -15,10 +15,10 @@ PROVIDERS = {
         "basic": "gpt-4.1-nano",
         "advanced": "gpt-4.1",
     },
-       "Google Gemini": {
+        "Google Gemini": {
         "secret": "GEMINI_API_KEY",
-        "basic": "gemini-3.6-flash",
-        "advanced": "gemini-pro-latest",
+        "basic": "gemini-flash-lite-latest",
+        "advanced": "gemini-3.8-flash",
     },
 }
 
